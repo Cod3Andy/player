e71929280150a20559acc90e055de8c4b5ad521f
Cod3Andy <derangedjk75@gmail.com>
2023-07-18 17:54:36 +0300

    last commit

--- script.js
+++ script.js
@@ -3,13 +3,17 @@ const playBtn = document.querySelector('#play')
 const prevBtn = document.querySelector('#prev')
 const nextBtn = document.querySelector('#next')
 const muteBtn = document.querySelector('#mute')
+const loopBtn = document.querySelector('#loop')
 const audio = document.querySelector('#audio')
 const progress = document.querySelector('.progress')
+const seek_slider = document.querySelector('.seek-slider')
 const progressContainer = document.querySelector('.progress-container')
 const title = document.querySelector('#title')
 const cover = document.querySelector('#cover')
-const slider = document.querySelector('.volume-slider');
-const progresVol = document.querySelector('progress');
+const slider = document.querySelector('.volume-slider')
+const progresVol = document.querySelector('progress')
+let curr_time = document.querySelector('.current-time')
+let total_duration = document.querySelector('.total-duration')
 // Song titles 
 const songs = ['Begin_again_Vera_Keyes', 'Put_Your_Head_On_My_Shoulder', 'Beyond_the_Sea']
 
@@ -57,6 +61,10 @@ function muteSong () {
     audio.volume > 0 ? (audio.volume = 0) & (slider.value = 0) : (audio.volume = 0.5) & (slider.value = 50)
     progresVol.value = slider.value
 }
+function loopSong () {
+    audio.loop === true ? audio.loop = false : audio.loop = true
+    playSong()
+}
 function setVolume() {
     audio.volume = slider.value / 100
     progresVol.value = slider.value
@@ -66,11 +74,34 @@ function updateProgress(e) {
     const progressPercent = (currentTime / duration) * 100
     progress.style.width = `${progressPercent}%`
 }
+function seekTo(e) {
+    const {duration, currentTime} = e.srcElement
+    const progressPercent = (currentTime / duration) * 100
+    seek_slider.value = progressPercent
+}
 function setProgress(e) {
     const width = this.clientWidth
     const clickX = e.offsetX
     const duration = audio.duration
     audio.currentTime = ((clickX / width) * duration)
+}
+//garbage code, doesn't work for now, gotta deal with it later.
+function setUpdate() {
+    let seekPosition = 0;
+    if(!isNaN(audio.duration)){
+        seekPosition = audio.currentTime * (100 / audio.duration);
+        seek_slider.value = seekPosition;
+        let currentMinutes = Math.floor(audio.currentTime / 60);
+        let currentSeconds = Math.floor(audio.currentTime - currentMinutes * 60);
+        let durationMinutes = Math.floor(audio.duration / 60);
+        let durationSeconds = Math.floor(audio.duration - durationMinutes * 60);
+        if(currentSeconds < 10) {currentSeconds = `0` + currentSeconds;}
+        if(durationSeconds < 10) {durationSeconds = `0` + durationSeconds;}
+        if(currentMinutes < 10) {currentMinutes = `0` + currentMinutes;}
+        if(durationMinutes < 10) {durationMinutes = `0` + durationMinutes;}
+        curr_time.textContent = currentMinutes + `:` + currentSeconds;
+        total_duration.textContent = durationSeconds + `:` + durationSeconds;
+    }   
 }
 // Event listeners
 playBtn.addEventListener('click', () => {
@@ -86,6 +117,8 @@ playBtn.addEventListener('click', () => {
 prevBtn.addEventListener('click', prevSong)
 nextBtn.addEventListener('click', nextSong)
 audio.addEventListener('timeupdate', updateProgress)
+audio.addEventListener('timeupdate', seekTo)
 progressContainer.addEventListener('click', setProgress)
 muteBtn.addEventListener('click', muteSong)
 audio.addEventListener('ended', nextSong)
+loopBtn.addEventListener('click', loopSong)
